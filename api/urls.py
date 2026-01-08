f18ce80aca1600c276562f5dd078f0734c13d8e7
TsaqifRadhitya <tsaqifradhitya@gmail.com>
2026-01-08 20:56:23 +0700

    fix : invalid order detail endpoint handler

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ProductAPIView, ProductDetailAPIView,OrderAPIView
+from .views import ProductAPIView, ProductDetailAPIView,OrderAPIView,OrderDetailApiView
 
 urlpatterns = [
     path("products/", ProductAPIView.as_view()),
     path("products/<int:pk>/", ProductDetailAPIView.as_view()),
     path("orders/", OrderAPIView.as_view()),
-    path("orders/<int:pk>", OrderAPIView.as_view()),
+    path("orders/<int:pk>", OrderDetailApiView.as_view()),
 ]
